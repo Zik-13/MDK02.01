0cc48e051e02b0ba5d2624ede86b4cadec108c1d
zikcruel@mail.ru <zikcruel@mail.ru>
2022-04-14 13:07:05 +0300

    update 14.04

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,13 +27,50 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            int num1 = Convert.ToInt32(first.Text);
+            int num2 = Convert.ToInt32(second.Text);
+            int num3 = Convert.ToInt32(frist.Text);
+            
+            if (num1 <= 0 || num2 <= 0 || num3 <= 0)
+            {
+                // неверные стороны треугольника
+                MessageBox.Show("Вы ввели не правильные данные / неверные стороны треугольника");
+                result.Text = "ERROR";
+            }
+            else if ( num1+num2 < num3 || num1+num3 < num2 || num2 + num3 < num1)
+            {
+                // неверные стороны треугольника
+                MessageBox.Show("Вы ввели не правильные данные / неверные стороны треугольника");
+                result.Text = "ERROR";
+            }
+            else
+            {
+                // все четко
+                if (num1 == num2 && num1 == num3 && num2 == num3)
+                {
+                    // равносторонний треугольник
+                    result.Text = "равносторонний";
+                }
+                else if (num1 == num2 || num1 == num3 || num2 == num3)
+                {
+                    // Равнобедренный
+                    result.Text = "равнобедренный";
+                }
+                else
+                {
+                    // Разносторнний
+                    result.Text = "разносторнний";
+                }
+            }
+
 
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             MainWindow mainWindow = new MainWindow();
-            mainWindow.Hide();
+            mainWindow.Show();
+            Hide();
             
         }
     }
