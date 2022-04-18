0e7b4fd92c14bd5a83d85f18edaac96a6966c626
zikcruel@mail.ru <zikcruel@mail.ru>
2022-04-18 13:05:31 +0300

    Many Fix

--- WpfApp1/Treygolnik.cs
+++ WpfApp1/Treygolnik.cs
@@ -16,7 +16,7 @@ namespace WpfApp1
             }
             else
             {
-                if ((num1 == num2 && num1 == num3 && num2 == num3)) 
+                if ((num1 == num2 && num1 == num3 && num2 == num3))
                 {
                     return "Равносторонний";
                 }
