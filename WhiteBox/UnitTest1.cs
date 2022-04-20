1af727107a7de0085701d054aeb8c5d0f72b17ec
zikcruel@mail.ru <zikcruel@mail.ru>
2022-04-20 09:52:24 +0300

    many fix

--- WhiteBox/UnitTest1.cs
+++ WhiteBox/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace WhiteBox
     public class UnitTest1
     {
         [TestMethod]
-        public void Ravnostoronyq_1_2_3()
+        public void Ravnostoronyq_5_6_7_Raznostoronniy()
         {
             int x = 5;
             int y = 6;
@@ -16,8 +16,31 @@ namespace WhiteBox
             string result ="Разносторнний";
             
             Treygolnik treygolnik = new Treygolnik();
+            Assert.AreEqual(result, treygolnik.Info(x, y, z));          
+        }
+
+        [TestMethod]
+        public void Ravnostoronyq_6_6_7_Ravnoberyniq()
+        {
+            int x = 6;
+            int y = 6;
+            int z = 7;
+            string result = "Равнобедренный";
+
+            Treygolnik treygolnik = new Treygolnik();
             Assert.AreEqual(result, treygolnik.Info(x, y, z));
-            
+        }
+
+        [TestMethod]
+        public void Ravnostoronyq_6_6_6_Ravnostoronyq()
+        {
+            int x = 6;
+            int y = 6;
+            int z = 6;
+            string result = "Равностороний";
+
+            Treygolnik treygolnik = new Treygolnik();
+            Assert.AreEqual(result, treygolnik.Info(x, y, z));
         }
     }
 }
